57c0572478a52cf174e3021223e938333968a3e7
Zach Chen <zachary.zh.chen@gmail.com>
2025-09-07 08:31:30 -0700

    init

---
+++ A_Painting_With_Two_Colors.py
@@ -0,0 +1,46 @@
+import sys
+import os
+
+problem_name = "cow_evo"
+
+input_file = f"{problem_name}.in"
+output_file = f"{problem_name}.out"
+
+if os.path.exists(input_file):
+    sys.stdin = open(input_file, "r")
+    sys.stdout = open(output_file, "w")
+
+
+def read_int():
+    """Reads a single integer from a line."""
+    return int(sys.stdin.readline())
+
+
+def read_str():
+    """Reads a single string from a line."""
+    return sys.stdin.readline().strip()
+
+
+def read_ints():
+    """Reads multiple integers from a line, separated by space."""
+    return list(map(int, sys.stdin.readline().split()))
+
+
+def read_strs():
+    """Reads multiple strings from a line, separated by space."""
+    return sys.stdin.readline().split()
+
+# helpers ends here
+t = read_int()
+for _ in range(t):
+    n, a, b = read_ints()
+    if a <= b:
+        if n % 2 == b % 2:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        if n % 2 == a % 2 and n % 2 == b % 2:
+            print('YES')
+        else:
+            print('NO')
